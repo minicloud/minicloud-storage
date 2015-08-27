9ef6b526dd039bad4de9e0e624225fa097c970a2
jim <jim@miniyun.cn>
2015-08-27 17:07:11 +0800

    find/merge v1.5

--- test/unit/file-helpers.js
+++ test/unit/file-helpers.js
@@ -1,7 +1,7 @@
 var request = require('co-supertest')
 var context = require('../context')
 var assert = require('assert')
-var fs =require('fs')
+var fs = require('fs')
 describe(' file-helpers.js', function() {
     this.timeout(10000)
     var app = null
@@ -24,16 +24,26 @@ describe(' file-helpers.js', function() {
     })
     it('merge miniyun v1.5', function*(done) {
         var cachePath = './test/test-files/merge/a/47/61/8d/22/47618d22b1830e42684579364e62f89000237433/new_47618d22b1830e42684579364e62f89000237433'
-        if(fs.existsSync(cachePath)){
+        if (fs.existsSync(cachePath)) {
             fs.unlinkSync(cachePath)
         }
         var paths = ['./test/test-files/merge/a', './test/test-files/merge/b', './test/test-files/merge/c']
         var path = yield fileHelpers.find(paths, '47618d22b1830e42684579364e62f89000237433')
         assert(fs.existsSync(path), true)
         var hash = yield fileHelpers.getFileSha1(path)
-        assert(hash,'47618d22b1830e42684579364e62f89000237433')
-        if(fs.existsSync(cachePath)){
+        assert(hash, '47618d22b1830e42684579364e62f89000237433')
+        if (fs.existsSync(cachePath)) {
             fs.unlinkSync(cachePath)
+        }
+        done()
+    })
+    it('getFilePath', function*(done) {
+        var paths = ['./test/test-files/merge/a', './test/test-files/merge/b', './test/test-files/merge/c']
+        var oldPath = yield fileHelpers.find(paths, '57618d22b1830e42684579364e62f89000237433')
+        if (!oldPath) {
+            var newPath = fileHelpers.getFilePath(paths, '57618d22b1830e42684579364e62f89000237433')
+            var existed = fs.existsSync(newPath)
+            assert(!existed, true) 
         }
         done()
     })
