5658c22e7ab9609a51f13ce4e557d42748d2d175
jim <jim@miniyun.cn>
2015-08-27 18:17:25 +0800

    file/upload

--- test/upload/upload.js
+++ test/upload/upload.js
@@ -4,13 +4,36 @@ var fsPlus = require('co-fs-plus')
 var context = require('../context')
 var fileHelpers = require('../../lib/file-helpers')
 var assert = require('assert')
+var path = require('path')
+    /**
+     * delete folder
+     * @param {String} sourcePath
+     * @param {String} aimPath   
+     * @return {Boolean}
+     * @api private
+     */
+var deleteFolder = function(filePath) {
+    var files = []
+    if (fs.existsSync(filePath)) {
+        files = fs.readdirSync(filePath)
+        files.forEach(function(file, index) {
+            var curPath = path.join(filePath, file)
+            if (fs.statSync(curPath).isDirectory()) { // recurse
+                deleteFolder(curPath)
+            } else { // delete file
+                fs.unlinkSync(curPath)
+            }
+        })
+        fs.rmdirSync(filePath)
+    }
+}
 describe(' files/upload', function() {
     this.timeout(10000)
     var app = null
     before(function*(done) {
         app = yield context.getApp()
-        fileHelpers.deleteFolder('./cache')
-        fileHelpers.deleteFolder('./data')
+        deleteFolder('./cache')
+        deleteFolder('./data')
         yield fsPlus.mkdirp('./cache')
         yield fsPlus.mkdirp('./data')
         return done()
@@ -21,12 +44,12 @@ describe(' files/upload', function() {
             .attach('file', './test/test-files/lt-1k.js')
             .expect(200)
             .end()
-        //assert data 
-        var path = yield fileHelpers.find(global.appContext.path,'47618d22b1830e42684579364e62f89000237433')
-        assert(fs.existsSync(path),true)
-        //assert cache
-        var files = yield fsPlus.walk('./cache') 
-        assert(files.length+1,1)
+            //assert data 
+        var path = yield fileHelpers.find(global.appContext.path, '47618d22b1830e42684579364e62f89000237433')
+        assert(fs.existsSync(path), true)
+            //assert cache
+        var files = yield fsPlus.walk('./cache')
+        assert(files.length + 1, 1)
         done()
     })
 
