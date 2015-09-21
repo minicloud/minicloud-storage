47fb480392d4adde33964b25d651bedd539c6c0c
jim <jim@miniyun.cn>
2015-09-21 10:17:03 +0800

    files/download->files/download/content files/thumbnail->files/thumbnail/content

--- test/file-download/file-download.js
+++ test/file-download/file-download.js
@@ -2,57 +2,57 @@ var request = require('co-supertest')
 var context = require('../context')
 var assert = require('assert')
 var md5 = require('md5')
-describe(' files/download', function() {
+describe(' files/download/content', function() {
     this.timeout(10000)
     var app = null
     before(function*(done) {
         app = yield context.getApp()
         return done()
     })
-    it(' /files/download 400', function*(done) {
+    it(' /files/download/content 400', function*(done) {
         var res = yield request(app)
-            .get('/api/v1/files/download')
+            .get('/api/v1/files/download/content')
             .type('json')
             .expect(400)
             .end()
         done()
     })
-    it(' /files/download 401', function*(done) {
+    it(' /files/download/content 401', function*(done) {
         var time = new Date().getTime()
         var res = yield request(app)
-            .get('/api/v1/files/download?hash=12345&name=1.doc&signature=1234567&time=' + time)
+            .get('/api/v1/files/download/content?hash=12345&name=1.doc&signature=1234567&time=' + time)
             .type('json')
             .expect(401)
             .end()
         res.body.error.should.equal('invalid_signature')
         done()
     })
-    it(' /files/download 409 session_timeout', function*(done) {
+    it(' /files/download/content 409 session_timeout', function*(done) {
         var sessionId = '1234'
         var time = new Date().getTime()
         time -= 24 * 60 * 60 * 1000 + 10
         var signature = md5(sessionId + time + global.appContext.safe_code)
         var res = yield request(app)
-            .get('/api/v1/files/download?hash=123456&name=1.doc&signature=' + signature + '&time=' + time)
+            .get('/api/v1/files/download/content?hash=123456&name=1.doc&signature=' + signature + '&time=' + time)
             .type('json')
             .expect(409)
             .end()
         res.body.error.should.equal('session_timeout')
         done()
     })
-    it(' /files/download 409 hash_not_exist', function*(done) {
+    it(' /files/download/content 409 hash_not_exist', function*(done) {
         var hash = '1234'
         var time = new Date().getTime()
         var signature = md5(hash + time + global.appContext.safe_code)
         var res = yield request(app)
-            .get('/api/v1/files/download?hash=' + hash + '&name=1.doc&signature=' + signature + '&time=' + time)
+            .get('/api/v1/files/download/content?hash=' + hash + '&name=1.doc&signature=' + signature + '&time=' + time)
             .type('json')
             .expect(409)
             .end()
         res.body.error.should.equal('hash_not_exist')
         done()
     })
-    it(' /files/download 200', function*(done) {
+    it(' /files/download/content 200', function*(done) {
         //upload file
         var sessionId = '1234'
         var time = new Date().getTime()
@@ -74,7 +74,7 @@ describe(' files/download', function() {
         var time = new Date().getTime()
         var signature = md5(hash + time + global.appContext.safe_code)
         yield request(app)
-            .get('/api/v1/files/download?hash=' + hash + '&name=测试.doc&signature=' + signature + '&time=' + time)
+            .get('/api/v1/files/download/content?hash=' + hash + '&name=测试.doc&signature=' + signature + '&time=' + time)
             .expect(200)
             .end()
         done()
--- test/file-thumbnail/file-thumbnail.js
+++ test/file-thumbnail/file-thumbnail.js
@@ -2,57 +2,57 @@ var request = require('co-supertest')
 var context = require('../context')
 var assert = require('assert')
 var md5 = require('md5')
-describe(' files/thumbnail', function() {
+describe(' files/thumbnail/content', function() {
     this.timeout(10000)
     var app = null
     before(function*(done) {
         app = yield context.getApp()
         return done()
     })
-    it(' /files/thumbnail 400', function*(done) {
+    it(' /files/thumbnail/content 400', function*(done) {
         var res = yield request(app)
-            .get('/api/v1/files/thumbnail')
+            .get('/api/v1/files/thumbnail/content')
             .type('json')
             .expect(400)
             .end()
         done()
     })
-    it(' /files/thumbnail 401', function*(done) {
+    it(' /files/thumbnail/content 401', function*(done) {
         var time = new Date().getTime()
         var res = yield request(app)
-            .get('/api/v1/files/thumbnail?hash=12345&name=1.png&signature=1234567&time=' + time)
+            .get('/api/v1/files/thumbnail/content?hash=12345&name=1.png&signature=1234567&time=' + time)
             .type('json')
             .expect(401)
             .end()
         res.body.error.should.equal('invalid_signature')
         done()
     })
-    it(' /files/thumbnail 409 session_timeout', function*(done) {
+    it(' /files/thumbnail/content 409 session_timeout', function*(done) {
         var sessionId = '1234'
         var time = new Date().getTime()
         time -= 24 * 60 * 60 * 1000 + 10
         var signature = md5(sessionId + time + global.appContext.safe_code)
         var res = yield request(app)
-            .get('/api/v1/files/thumbnail?hash=123456&name=1.png&signature=' + signature + '&time=' + time)
+            .get('/api/v1/files/thumbnail/content?hash=123456&name=1.png&signature=' + signature + '&time=' + time)
             .type('json')
             .expect(409)
             .end()
         res.body.error.should.equal('session_timeout')
         done()
     })
-    it(' /files/thumbnail 409 hash_not_exist', function*(done) {
+    it(' /files/thumbnail/content 409 hash_not_exist', function*(done) {
         var hash = '1234'
         var time = new Date().getTime()
         var signature = md5(hash + time + global.appContext.safe_code)
         var res = yield request(app)
-            .get('/api/v1/files/thumbnail?hash=' + hash + '&name=1.doc&signature=' + signature + '&time=' + time)
+            .get('/api/v1/files/thumbnail/content?hash=' + hash + '&name=1.doc&signature=' + signature + '&time=' + time)
             .type('json')
             .expect(409)
             .end()
         res.body.error.should.equal('hash_not_exist')
         done()
     })
-    it(' /files/thumbnail 200', function*(done) {
+    it(' /files/thumbnail/content 200', function*(done) {
         //upload file
         var sessionId = '1234'
         var time = new Date().getTime()
@@ -75,7 +75,7 @@ describe(' files/thumbnail', function() {
         var time = new Date().getTime()
         var signature = md5(hash + time + global.appContext.safe_code)
         var res = yield request(app)
-            .get('/api/v1/files/thumbnail?hash=' + hash + '&name=test.jpg&signature=' + signature + '&time=' + time + '&size=w32h32')
+            .get('/api/v1/files/thumbnail/content?hash=' + hash + '&name=test.jpg&signature=' + signature + '&time=' + time + '&size=w32h32')
             .expect(200)
             .end() 
         done()
