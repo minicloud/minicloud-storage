75cfa33aa0735836e2651f3826df51789ea742e4
jim <jim@miniyun.cn>
2015-09-21 11:14:34 +0800

    Integration with minicloud

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-/**
- * Module dependencies.
- */
-var co = require('co')
-//start app
-co.wrap(function*(){
-	var app = yield require("./lib/loader/app-loader")()	
-	app.listen(global.appContext.port)
-	console.log(global.appContext.port+" is running!")	
-})()
+module.exports = require('./lib')
